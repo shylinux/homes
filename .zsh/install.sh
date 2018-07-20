e6b1b1dc02bb995b689f9fe79e35fc80f81e4cf6
shaoying <shylinux@icloud.com>
2018-07-20 17:28:57 +0800

    add tmux install

--- .zsh/install.sh
+++ .zsh/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# if [ `uname` = "Darwin" ] ; then
-# 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# fi
-#
+if [ `uname` = "Darwin" ] ; then
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 tar xvf .vim/bundle.tar.gz -C .vim
 tar xvf .zsh/oh-my-zsh.tar.gz
 touch .vim_local
